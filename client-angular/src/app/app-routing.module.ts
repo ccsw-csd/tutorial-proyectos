2a945b85a0f1edb38afcf7d28d388efe9feb38bd
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2023-02-21 11:54:09 +0100

    Cambio de nombres de proyectos y añadido nodejs

---
+++ client-angular/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthorListComponent } from './author/author-list/author-list.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { GameListComponent } from './game/game-list/game-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/games', pathMatch: 'full'},
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'authors', component: AuthorListComponent },
+  { path: 'games', component: GameListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ client-angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { CategoryModule } from './category/category.module';
+import { AuthorModule } from './author/author.module';
+import { GameModule } from './game/game.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    CategoryModule,
+    AuthorModule,
+    GameModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
